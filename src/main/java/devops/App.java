37267f2a97cf23298e1abd41b9e58d4dff74dafb
rakesh <rakeshk.singh29@gmail.com>
2021-03-05 18:55:52 +0000

    v5

--- src/main/java/devops/App.java
+++ src/main/java/devops/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello India i love you!" );
+        System.out.println( "Hello India i love u!" );
     }
 }
